c5118df07c5d1660fc92f947283316a233e82521
HDerwyn <hderwynbdo@gmail.com>
2024-06-20 01:28:26 -0400

    test comment for integration

--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -3,7 +3,7 @@ const request = require("supertest");
 const server = require("../../src/interfaces/http/server");
 
 describe("GET /integration", () => {
-  test("should respond with a 401 status code", async () => {
+  test("the request is being sent without auth header so it should respond with a 401 status code", async () => {
     const response = await request(server).get("/integration").send();
     expect(response.statusCode).toBe(401);
   });
